716e1c84b9b590be5063c893e948518a2d54fe47
lopesrichard <contato.richardlopes@outlook.com>
2019-06-14 21:09:28 -0300

    Revisão Expressões Regulares

--- src/Arrays.php
+++ src/Arrays.php
@@ -15,9 +15,9 @@ class Arrays implements \ArrayAccess, \IteratorAggregate, \Countable, Sortable, 
     protected const BREAK    = 'break';
     protected const CONTINUE = 'continue';
 
-    protected const KEY_NOTATION      = '/^\w+$/';
-    protected const DOT_NOTATION      = '/(?<=\.|^)(?<key>\w+)(?=\.|$)/';
-    protected const INTERVAL_NOTATION = '/^(?<start>\w+):(?<end>\w+)$/';
+    protected const KEY_NOTATION      = '/^[\w-]+$/';
+    protected const DOT_NOTATION      = '/(?<=\.|^)(?<key>[\w-]+)(?=\.|$)/';
+    protected const INTERVAL_NOTATION = '/^(?<start>\d+):(?<end>\d+)$/';
 
     protected $content;
     protected $length;
